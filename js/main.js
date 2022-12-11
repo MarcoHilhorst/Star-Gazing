4bf47f909aff99c5e27cbc50c8399e2280872711
Marco <marco-hilhorst@hotmail.co.nz>
2022-12-11 16:02:05 +1300

    first commit

---
+++ js/main.js
@@ -0,0 +1,92 @@
+document.querySelector('button').addEventListener('click', choose)
+
+document.querySelector('.fwd').addEventListener('click', dateFoward)
+document.querySelector('.bck').addEventListener('click', dateBackward)
+document.querySelector('.rndm').addEventListener('click', searchRandomDate)
+
+let selectedDate = new Date()
+
+
+function choose(){
+  let choice = document.querySelector('input').value
+  selectedDate = new Date(choice)
+  getFetch(choice)
+}
+
+function getFetch(date){
+  let url = `https://api.nasa.gov/planetary/apod?api_key=XGX2aMHaHfOJY56GvNaxSX67wRLF57eyjl2EkOTx&date=${date}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if (data.media_type === 'image'){
+          document.querySelector('img').src = data.hdurl
+        } else if (data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+        }
+        
+        document.querySelector('h3').innerText = data.explanation
+        
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+    }
+
+function dateFoward(){
+  var myDateFwd = selectedDate
+  var zeros = function(val) { 
+  var str = val.toString()
+  return (str.length < 2) ? "0" + str : str // Adds zero infront of number for single digits E.G 9 -> 09
+  }
+  myDateFwd.setDate(myDateFwd.getDate() + 1)
+    let y = myDateFwd.getFullYear()
+    let m = myDateFwd.getMonth() + 1 // january is treated as 0 is JS
+    let d = myDateFwd.getDate()
+    let dateString = [y, zeros(m), zeros(d)].join("-"); 
+    getFetch(dateString)
+}
+
+function dateBackward(){
+  var myDateBack = selectedDate
+  var zeros = function(val) { 
+    var str = val.toString()
+    return (str.length < 2) ? "0" + str : str // Adds zero infront of number for single digits E.G 9 -> 09
+  }
+  myDateBack.setDate(myDateBack.getDate() - 1)
+    let y = myDateBack.getFullYear()
+    let m = myDateBack.getMonth() + 1 // january is treated as 0 is JS
+    let d = myDateBack.getDate()
+    let dateString = [y, zeros(m), zeros(d)].join("-") // converts the date into the format required by the API
+    getFetch(dateString)
+}
+
+function searchRandomDate(){
+  let myDateRandom = generateRandomDate(new Date(1995, 5, 20), new Date())
+  selectedDate = myDateRandom
+  console.log(selectedDate)
+  var zeros = function(val) { 
+    var str = val.toString()
+    return (str.length < 2) ? "0" + str : str // Adds zero infront of number for single digits E.G 9 -> 09
+  }
+  myDateRandom.setDate(myDateRandom.getDate() - 1)
+    let y = myDateRandom.getFullYear()
+    let m = myDateRandom.getMonth() + 1 // january is treated as 0 is JS
+    let d = myDateRandom.getDate()
+    let dateString = [y, zeros(m), zeros(d)].join("-") // converts the date into the format required by the API
+    getFetch(dateString)
+
+}
+
+function generateRandomDate(start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+}
+
+// console.log(randomDate(new Date(2012, 0, 1), new Date()))
+
+
+
+
+
+//https://api.nasa.gov/planetary/apod?api_key=XGX2aMHaHfOJY56GvNaxSX67wRLF57eyjl2EkOTx
